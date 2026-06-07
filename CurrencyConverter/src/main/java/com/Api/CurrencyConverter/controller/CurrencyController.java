b82f54d15f373dd0104bf4ac325cee51b2d3e964
Avanish0210 <avanishsingh0210@gmail.com>
2026-06-07 15:13:08 +0530

    added log

--- CurrencyConverter/src/main/java/com/Api/CurrencyConverter/controller/CurrencyController.java
+++ CurrencyConverter/src/main/java/com/Api/CurrencyConverter/controller/CurrencyController.java
@@ -2,21 +2,26 @@ package com.Api.CurrencyConverter.controller;
 
 import com.Api.CurrencyConverter.service.ConverterService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/currencyConverter")
+@Slf4j
 public class CurrencyController {
     private final  ConverterService converterService;
 
     @GetMapping("/convert")
     public ResponseEntity<Double> getCurrency(@RequestParam(value = "amount", required = true) Double amount){
+        log.info("this is CurrencyController");
         return ResponseEntity.ok(converterService.convertCurrency(amount));
     }
     //currencyConverter/convert?fromCurrency=INR&ToCurrency=USD&amount=99.00
     //currencyConverter/convert?amount=99.00
+
+
 
 
 }
